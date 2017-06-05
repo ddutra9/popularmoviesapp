84d1a6073df815fd841f81e2bddd4e151aa89a4a
donato <ddutra9@gmail.com>
2017-06-05 11:35:08 -0300

    criacao de assynctask e classe movie

---
+++ app/src/main/java/com/ddutra9/popularmoviesapp/model/Movie.java
@@ -0,0 +1,56 @@
+package com.ddutra9.popularmoviesapp.model;
+
+import java.util.Date;
+
+/**
+ * Created by donato on 05/06/17.
+ */
+
+public class Movie {
+
+    private String title;
+    private String overview;
+    private Double voteAverage;
+    private Date releaseDate;
+    private String posterPath;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+}
